48116dcb1f11e43154aeb22e73b40fefa93afa9e
Nate Abele <nate.abele@gmail.com>
2014-08-25 08:55:41 -0400

    Module setup, better bindings, state defs

--- js/base.js
+++ js/base.js
@@ -1,5 +1,32 @@
 "use strict";
 
-function NavigationController($scope) {
+var SockDrawer = angular.module("sockdrawer", [
+  'ui.bootstrap',
+  'ui.router',
+  'ur.model',
+  'ur.scaffold'
+]);
+
+SockDrawer.config(function($locationProvider, $stateProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state("apps", {
+    url: "/"
+  }).state("apps.messages", {
+    url: "/messages"
+  }).state("apps.photos", {
+    url: "/photos"
+  }).state("apps.files", {
+    url: "/files"
+  });
+
+});
+
+SockDrawer.run(function($state, $location) {
+  $state.go("apps");
+});
+
+SockDrawer.controller("NavigationController", function($scope, $state) {
   $scope.user = { name: "Test" };
-}
+});
